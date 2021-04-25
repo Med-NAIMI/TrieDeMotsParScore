7f055fbff289c0d755052a436f26fa3c6575ce24
Med-NAIMI <56475732+Med-NAIMI@users.noreply.github.com>
2021-04-25 19:37:02 +0200

    Add files via upload

---
+++ TrieDeMotsParScore/ProblemSolving/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("entrer le string donnat les characteres miniscules et leurs scores, sous forme de \"a 1 b 3 c 3 d 2 e 1 f 4 g 2 u 1\"");
+            string s = Console.ReadLine();//chars et leurs scores
+
+
+            Console.WriteLine("vous allez entrer une succession de mots de w");
+
+            string arret="oui";
+            string s1;//variable intermediare pour aider a l'insertion dans la liste des mots 
+            List<string> w = new List<string>();
+            //la liste des mots qu'on va classifier suivant les scores de chars
+            while (arret=="oui")
+            {
+                Console.WriteLine("svp, entrez un mots de votre choix!");
+                s1= Console.ReadLine();
+                w.Add(s1);//ajout d'un mot
+                Console.WriteLine("y'a t il un autre mot que vous vouler ajouter? ");
+                Console.WriteLine("tapper oui pour continuer! ");
+                arret = Console.ReadLine();
+
+            }
+
+            Solution.AfficherMots(s,w);
+
+            Console.WriteLine("svp, entrer un mots de votre choix!");//pour conserver l affichage
+
+            Console.Read();
+          
+        }
+    }
+}
---
+++ TrieDeMotsParScore/ProblemSolving/Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    abstract class Solution
+    {
+        //la méthode qui va afficher les mots données en ordre décroissant des scores
+        public static void AfficherMots(String S,List<String> w)
+        {
+
+            //le dictionnaire contenant les charactere et leurs scores
+            Dictionary<char, int> charScores = extractionScores(S);//extractionScores() la fonction qui affecte a chaque char son score
+
+
+            //le dictionnaire contenant les strings et leurs scores
+            Dictionary<String, int> stringScores;
+            stringScores = affectationScores(w,charScores);//affectationScores() la fonction qui affecte a chaque string son score
+
+
+            //selection sur les string données dans w en ordre décroissant
+            var words = stringScores.OrderByDescending(p => p.Value).Select(p => p.Key).ToList();
+
+            foreach(string s in words)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+
+        private static Dictionary<string, int> affectationScores(List<string> w, Dictionary<char, int> charScores)
+        {
+            //initialisation du score de chaque string contenu dans w à 0
+            int score = 0;
+
+
+            //dictionnaire qui ressemble les chaines et leurs scores
+            Dictionary<string, int> stringScores = new Dictionary<string, int>();
+
+
+            foreach(string s in w)
+            {
+                foreach (char c in s)
+                {
+                    if (charScores.ContainsKey(c))
+                        score += charScores[c];
+                    //si le string S données dans l'exo contient le char dans string de w, on ajoute son score
+                    
+                    else
+                        score += 0;
+                    //si le string S données dans l'exo ne contient pas le char dans string de w, son score est 0
+
+                }
+                
+                stringScores.Add(s,score);//ajouter le string traité et son score
+                score = 0;//remmetre score à 0
+            }
+            return stringScores;//return un dictionnaire contenant (string, score)
+        }
+
+        private static Dictionary<char, int> extractionScores(string s)
+        {
+            //dictionnaire qui ressemble les characteres et leurs scores
+            Dictionary<char, int> charScores = new Dictionary<char, int>();
+            
+            
+            for(int i=0; i<s.Length;i+=4)
+                {
+                //construction du dictionnaire qui ressemble les characteres et leurs scores
+                    charScores.Add(s[i],(int)Char.GetNumericValue(s[i+2]));
+                }
+            
+            return charScores;//return du dictionnaure construit
+        }
+    }
+}
